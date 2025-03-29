6396d941710b5685211f9f1def0c07d780058247
yoel-mc <yoel@mulaicoding.id>
2025-03-29 21:19:02 +0700

    merge

--- entity/church.go
+++ entity/church.go
@@ -5,7 +5,6 @@ import (
 )
 
 type Church struct {
-<<<<<<< HEAD
 	ID          uuid.UUID `gorm:"type:char(36);primary_key;"`
 	Name        string    `gorm:"type:varchar(255);not null"`
 	Address     string    `gorm:"type:text;not null"`
@@ -14,18 +13,6 @@ type Church struct {
 	Website     string    `gorm:"type:varchar(255)"`
 	KabupatenID uint      `gorm:"type:int;not null"`
 	Kabupaten   Kabupaten `gorm:"foreignKey:KabupatenID"`
-=======
-	ID         uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Name       string    `gorm:"type:varchar(255);not null"`
-	Address    string    `gorm:"type:text;not null"`
-	Phone      string    `gorm:"type:varchar(20)"`
-	Email      string    `gorm:"type:varchar(255)"`
-	Website    string    `gorm:"type:varchar(255)"`
-	CityID     uuid.UUID `gorm:"type:uuid;not null"`
-	ProvinceID uuid.UUID `gorm:"type:uuid;not null"`
-	City       City      `gorm:"foreignKey:CityID"`
-	Province   Province  `gorm:"foreignKey:ProvinceID"`
->>>>>>> 5ed98ee3f618ce09f59ee7b2565092ff36194b0f
 
 	Timestamp
 }
